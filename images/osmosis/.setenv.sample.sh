6bce8306b8551d3f050da2d623e2c4ea1e8dd036
Tommy Guy <riguy@microsoft.com>
2021-10-29 15:51:24 -0700

    updated dockerfile and run script

---
+++ images/osmosis/.setenv.sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# copy this file to .setenv.sh
+
+# Sets secrets like connection strings
+export AZURE_STORAGE_CONNECTION_STRING="SETME"
--- images/osmosis/osmosis_driver.sh
+++ images/osmosis/osmosis_driver.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-# we'll use azcopy for all of this, because we can azcopy with a service principal.
-# to use, create SP and give Azure Batch that role.
+# sets some vars
+source /user/local/bin/setenv.sh
+echo $ENVS_SET
 
-# copy file 
+# blob file 
 azcopy copy "https://raweusroutefinder.blob.core.windows.net/rawpbf/na.us.wa.osm.pbf" "/tmp/na.osm.pbf"
+
+echo "blob of incoming pbf done"
+ls -lh /tmp
 
 # run osmosis 
 # read once, filter, then write two sets of files
 # one boundary set
 # many split out files for runnable ways.
 osmosis --read-pbf-fast file=/tmp/na.osm.pbf workers=8 \
-    --tf accept-ways highway=track,residential,footpath,footway,path,service,tertiary,cycleway,primary \
     --tf reject-ways golf=cartpath \
     --tf reject-ways access=permit,private \
     --tf reject-ways service=driveway,parking_aisle \
-    --tf accept-relations boundary=administrative \
     --tf reject-relations access=private \
-    --used-node \
-    --write-xml /tmp/highway.xml
+    --tf reject-relations landuse=military \
+    --tee 2 \
+    \
+        --tf accept-relations boundary=administrative,national_park \
+        --used-way \
+        --used-node \
+        --write-xml /tmp/boundaries.xml \
+    \
+        --tf accept-ways highway=track,residential,steps,footpath,footway,path,tertiary,cycleway,primary \
+        --tf reject-ways service=driveway \
+        --used-node \
+        --tee 8 \
+        --bounding-box right=-114 bottom=40.5 completeWays=yes --write-xml /tmp/bbox_1_1.xml \
+        --bounding-box left=-114 right=-89.5 bottom=40.5 completeWays=yes --write-xml /tmp/bbox_1_2.xml \
+        --bounding-box left=-89.5 right=-77.7 bottom=40.5 completeWays=yes --write-xml /tmp/bbox_1_3.xml \
+        --bounding-box left=-77.7 bottom=40.5 completeWays=yes --write-xml /tmp/bbox_1_4.xml \
+        --bounding-box top=40.5 right=-114 completeWays=yes --write-xml /tmp/bbox_2_1.xml \
+        --bounding-box left=-114 top=40.5 right=-89.5 completeWays=yes --write-xml /tmp/bbox_2_2.xml \
+        --bounding-box left=-89.5 top=40.5 right=-77.7 completeWays=yes --write-xml /tmp/bbox_2_3.xml \
+        --bounding-box left=-77.7 top=40.5 completeWays=yes --write-xml /tmp/bbox_2_4.xml
 
 # Logs
 echo "Done with Osmosis processing with these files"
 
 ls -hl /tmp
 
-# copy output files
+# blob output files
+az storage blob upload-batch -d rawpbf -s /tmp --pattern b*.xml --destination-path rawxml/
 
+echo "Done with blob"
